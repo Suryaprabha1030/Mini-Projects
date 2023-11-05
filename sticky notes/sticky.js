bf12ed07c1a8328e3bc316802c1f79a550a6dc52
Suryaprabha Chandrasekar <149596831+Suryaprabha1030@users.noreply.github.com>
2023-11-05 10:56:27 +0530

    v1

--- sticky notes/sticky.js
+++ sticky notes/sticky.js
@@ -1,59 +1,18 @@
-const content = document.querySelector(".content");
-const btnAdd = document.querySelector(".addbtn");
+const notesContainer = document.querySelector(".notes-container");
+const addBtn = document.querySelector(".addbtn");
+let notes = document.querySelectorAll(".input-box");
 
-function getAppUpdate() {
-  return JSON.parse(localStorage.getItem("surya") || [""]);
-}
-
-getAppUpdate().forEach((element) => {
-  const textElement = createElement(element.id, element.content);
-  content.insertBefore(textElement, btnAdd);
+addBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
 });
 
-function createElement(id, content) {
-  const textElement = document.createElement("textArea");
-  textElement.classList.add("sticky");
-  textElement.value = content;
-  textElement.placeholder = "Enter here";
-  textElement.addEventListener("change", () => {
-    updateNote(id, textElement.value);
-  });
-
-  textElement.addEventListener("dblclick", () => {
-    const check = confirm("Are you sure to delete");
-    if (check) deleteNote(id, textElement);
-  });
-
-  return textElement;
-}
-
-function addSticky() {
-  const notes = getAppUpdate();
-  const noteOfObject = {
-    id: Math.floor(Math.random() * 10000),
-    content: "",
-  };
-  const textElement = createElement(noteOfObject.id, noteOfObject.content);
-  content.insertBefore(textElement, btnAdd);
-  notes.push(noteOfObject);
-  saveNotes(notes);
-}
-
-btnAdd.addEventListener("click", () => addSticky());
-
-function saveNotes(notes) {
-  localStorage.setItem("surya", JSON.stringify(notes));
-}
-
-function updateNote(id, content) {
-  const notes = getAppUpdate();
-  const updateElement = notes.filter((note) => note.id == id)[0];
-  updateElement.content = content;
-  saveNotes(notes);
-}
-
-function deleteNote(id, textElement) {
-  const notes = getAppUpdate().notes.filter((note) => note.id != id)[0];
-  saveNotes(notes);
-  content.removeChild(textElement);
-}
+notesContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
+});
